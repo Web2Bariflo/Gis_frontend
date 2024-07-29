8c8587d2ea6b2b07c5a88fd9958edb8bbbbae23d
web2bariflo <web2.bariflo@outlook.com>
2024-07-29 18:38:01 +0530

    change server ip

--- src/URL.jsx
+++ src/URL.jsx
@@ -3,11 +3,11 @@ function URL() {
     // URL = "http://20.244.51.146:8000"
     // URL = "http://20.244.51.20"
     //URL = "http://192.168.1.31:8001" //Pradip//
-     URL = "http://192.168.1.30:8000" //Tapas/
+     // URL = "http://192.168.1.30:8000" //Tapas/
     // URL = "http://13.201.249.63:8000"
     // URL = "http://192.168.0.144:8000" //satyajit
     // URL = "http://20.244.110.108:8000" //server//
-    // URL = "https://waterbg.bc-pl.com" //server//
+     URL = "https://waterbg.bc-pl.com" //server//
 
     return URL
 }
